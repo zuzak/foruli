03e509505a87f1df7db96cbabfbc6dbfd0930665
Douglas Gardner <douglas@chippy.ch>
2013-10-14 23:09:52 +0000

    remove primo

--- app/routes/viewbook.js
+++ app/routes/viewbook.js
@@ -2,7 +2,6 @@ var ensureLogin = require('connect-ensure-login').ensureLoggedIn
 var oL = 'http://openlibrary.org/api/books?format=json&jscmd=data&bibkeys=ISBN:'
 var request = require('request')
 var UserBook = require('../schemas/userbook')
-var aberprimo = require('aberprimo')
 
 module.exports = function (app) {
 
@@ -42,18 +41,15 @@ module.exports = function (app) {
           username: req.user.username,
           isbn: req.params.isbn
         }, function (err, associated) {
-          aberprimo.get(req.params.isbn, function (primo) {
-            res.render('book',{
-              book: data,
-              user: req.user,
-              pretty: true,
-              active: 'books',
-              title: title,
-              isbn: req.params.isbn,
-              associated: associated,
-              primo: primo,
-              translate: aberprimo.translate
-            })
+          res.render('book',{
+            book: data,
+            user: req.user,
+            pretty: true,
+            active: 'books',
+            title: title,
+            isbn: req.params.isbn,
+            associated: associated,
+            translate: aberprimo.translate
           })
         })
       })
